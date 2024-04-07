99fc491832627c70c70b4a869a1e13a4b9c87a64
Alexia Stephan <alexia0stephan@gmail.com>
2024-04-07 09:37:17 +0200

    résolution beug affichage

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -4,7 +4,7 @@ function Background({ children, mode }) {
   const nightColor = "bg-gray-900";
   const dayColor = "bg-teal-50";
   const bgColor = mode === "night" ? nightColor : dayColor;
-  return <div className={`${bgColor} min-h-screen w-screen`}>{children}</div>;
+  return <div className={`${bgColor}`}>{children}</div>;
 }
 
 export default Background;
