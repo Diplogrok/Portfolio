ffd1a7ad1c5e8d009f5748af03a4f4b0ae981c30
Alexia Stephan <alexia0stephan@gmail.com>
2024-04-10 18:42:16 +0200

    ajout des png slider

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,48 +2,16 @@ import React, { useState } from "react";
 import { useModeContext } from "../context/ModeContext";
 import data from "../assets/datas/slider.json";
 
-// SVG pour la flèche droite
-const RightArrowIcon = ({ arrowColor }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 512"
-    className="w-16 h-16 ">
-    <path
-      d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z "
-      fill={arrowColor}
-    />
-  </svg>
-);
-
-// SVG pour la flèche gauche
-const LeftArrowIcon = ({ arrowColor }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 512"
-    className="w-16 h-16">
-    <path
-      d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"
-      fill={arrowColor}
-    />
-  </svg>
-);
-
 function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const isMobile = window.innerWidth < 768;
   const { getColor } = useModeContext();
 
-  const arrowColor = getColor();
+  const bulletColor = getColor();
 
-  const handleSlideChange = (direction) => {
-    const lastIndex = data.length - 1;
-    let newIndex;
-    if (direction === "next") {
-      newIndex = currentSlide === lastIndex ? 0 : currentSlide + 1;
-    } else {
-      newIndex = currentSlide === 0 ? lastIndex : currentSlide - 1;
-    }
-    setCurrentSlide(newIndex);
+  const handleBulletClick = (index) => {
+    setCurrentSlide(index);
   };
 
   return (
@@ -54,7 +22,13 @@ function Slider() {
         {data.map((item, index) => (
           <div key={item.id} className="min-w-full flex justify-center">
             <img
-              src={hoveredIndex === index ? item.focusPicture : item.picture}
+              src={
+                hoveredIndex === index
+                  ? isMobile
+                    ? item.mobilePicture
+                    : item.focusPicture
+                  : item.picture
+              }
               alt={`Slide ${item.id}`}
               className="rounded-lg cursor-pointer border border-purple-300"
               onMouseEnter={() => setHoveredIndex(index)}
@@ -67,23 +41,14 @@ function Slider() {
           </div>
         ))}
       </div>
-      <div
-        onClick={() => handleSlideChange("prev")}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 cursor-pointer">
-        <LeftArrowIcon arrowColor={arrowColor} />
-      </div>
-      <div
-        onClick={() => handleSlideChange("next")}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer">
-        <RightArrowIcon arrowColor={arrowColor} />
-      </div>
-      <div className="absolute bottom-2 left-0 right-0 flex justify-center">
+      <div className="pt-4 flex justify-center">
         {data.map((_, index) => (
           <span
             key={index}
-            className={`w-3 h-3 rounded-full mx-2 border ${
-              currentSlide === index ? "bg-gray-900" : "bg-gray-400"
+            className={`w-4 h-4 rounded-full mx-1 cursor-pointer ${
+              currentSlide === index ? "bg-gray-800" : "bg-gray-400"
             }`}
+            onClick={() => handleBulletClick(index)}
           />
         ))}
       </div>
