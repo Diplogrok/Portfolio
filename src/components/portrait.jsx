9ff817bf2b5399379549fadd818a71ef0260ac5d
Alexia Stephan <alexia0stephan@gmail.com>
2024-04-12 15:26:26 +0200

    chgt emplacements img

--- src/components/portrait.jsx
+++ src/components/portrait.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import portraitImg from "/Portrait.jpg";
+import portraitImg from "/images/Portrait.jpg";
 import Description from "./Description";
 import data from "../assets/datas/text.json";
 import Legend from "./Legend";
