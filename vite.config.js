5357590d5036c35d19681a0afd02fc794cc7788b
Alexia Stephan <alexia0stephan@gmail.com>
2024-04-12 16:38:33 +0200

    modification vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 // vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: import.meta.env.VITE_APP_LOCAL ? "/" : "/Portfolio/",
-  plugins: [react()],
-  server: {
-    host: true,
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: process.env.VITE_APP_LOCAL ? "/" : "/Portfolio",
+    plugins: [react()],
+    server: {
+      host: true,
+    },
+  });
+};
